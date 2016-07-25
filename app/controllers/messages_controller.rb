4d4814196f3d79751c369bd4273a931aa21c5efb
IEgor Dorovskykh <egor.odessa@gmail.com>
2016-07-25 13:51:35 +0300

    msg pagination

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,18 +5,28 @@ class MessagesController < ApplicationController
 
     @receiver_user = User.find(params[:user_id])
 
-    @messages = []
+    # @messages = []#.page(params[:page])
     # binding.pry
-    unless current_user.sent_messages.empty?
-      @messages += current_user.sent_messages.where(receiver: @receiver_user)#.to_ary
-    end
-    unless @receiver_user.sent_messages.empty?
-      @messages += @receiver_user.sent_messages.where(receiver: current_user)#.to_ary
-    end
+    # unless current_user.sent_messages.empty?
+    #   @messages += current_user.sent_messages.where(receiver: @receiver_user)#.to_ary
+    # end
+    # unless @receiver_user.sent_messages.empty?
+    #   @messages += @receiver_user.sent_messages.where(receiver: current_user)#.to_ary
+    # end
+    messages_current_user = Message.where(sender_id: current_user.id,
+                              receiver_id: @receiver_user.id)
+    messages_current_receiver = Message.where(sender_id: @receiver_user.id,
+                              receiver_id: current_user.id)
+    # binding.pry
+    @messages = (messages_current_user + messages_current_receiver).sort
+    # binding.pry
+    @messages = Kaminari.paginate_array(@messages).page(params[:page])
 # binding.pry
     # @messages = current_user.sent_messages.where(receiver_id: @receiver.id) +
     #             @receiver_user.sent_messages.where(receiver_id: current_user.id)
     # # end
+    # @messages = @messages.page(params[:page])
+
 
     @message = Message.new
     # @messages =  Message.private_messages_array(@receiver.id)
