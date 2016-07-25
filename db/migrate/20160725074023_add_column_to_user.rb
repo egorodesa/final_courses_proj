b1247ed770a18a20bca5649adb9ec3ee5a8fef15
IEgor Dorovskykh <egor.odessa@gmail.com>
2016-07-25 10:56:41 +0300

    user model advise and paperclip

---
+++ db/migrate/20160725074023_add_column_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :phone_number, :string
+    add_column :users, :address, :string
+    add_column :users, :date_of_birth, :date
+    add_column :users, :age, :integer
+
+  end
+end
